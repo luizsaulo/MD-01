72ae12650959f792eeadddf5b7772160ec9efbe8
Luiz <luizsaulo@yahoo.com.br>
2022-06-23 17:17:10 -0300

    finishing the Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import styles from './Header.module.css';
 
+import igniteLogo from '../assets/Ignite-logo.svg';
+
 export default function Header() {
   return (
     <header className={styles.header}>
-        <strong >Ignite Feed</strong>
+        <img src={igniteLogo} alt='Ignite Logo' />
     </header>
    
   )
