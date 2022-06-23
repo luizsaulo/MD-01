8fb9f92b2b248df0818bf047b3d3f750a9fd9f91
Luiz <luizsaulo@yahoo.com.br>
2022-06-23 16:42:17 -0300

    creation of a few files to start things, including a global css

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
+import Header from "./components/Header"
+
+import './global.css';
+
 function App() {
   
   return (
-    <h1>Hello World</h1>
+    <div>
+      <Header />
+      <h1>Hello World</h1>
+    </div>
+    
   )
 }
 
 export default App
+
