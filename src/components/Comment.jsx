b2709cb5711cfc4cdb16db8e22503f201b42998b
Luiz <luizsaulo@yahoo.com.br>
2022-06-27 11:12:42 -0300

    creation of the comment component

---
+++ src/components/Comment.jsx
@@ -0,0 +1,37 @@
+import { Trash, ThumbsUp } from 'phosphor-react';
+import React from 'react'
+import styles from './Comment.module.css';
+
+export default function Comment() {
+  return (
+    
+    <div className={styles.comment}>
+      <img src='https://github.com/luizsaulo.png' />
+
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Luiz Saulo</strong>
+            <time title='June 27th 2022 at 11:01h' dateTime='2022-06-27'>Publicado há cerca de 1h</time>
+            </div>
+
+            <button title='Deletar comentário'>
+              <Trash size={20} />
+            </button>
+          </header>
+
+          <p>Muito bom, parabéns!</p>
+        </div>
+
+      </div>
+      <footer>
+        <button>
+          <ThumbsUp />
+          Aplaudir <span>20</span>
+        </button>
+      </footer>
+      
+    </div>
+  )
+}
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Comment from './Comment';
 import styles from './Post.module.css';
 
 export default function Post() {
@@ -37,6 +38,12 @@ export default function Post() {
                 <button type='submit'>Publicar</button>
             </footer>
         </form>
+
+        <div className={styles.commentList}>
+            <Comment />
+            <Comment />
+            <Comment />
+        </div>
     </article>
   )
 }
