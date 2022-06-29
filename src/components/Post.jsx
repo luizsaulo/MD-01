c8ec5c4ce1d1f260d5f8df3978fa0f06528931f3
Luiz <luizsaulo@yahoo.com.br>
2022-06-29 09:15:44 -0300

    correcting issues on the list properties in 2 different components - the addition of a key to 3 different map lists

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -53,11 +53,12 @@ export default function Post({ author, publishedIn, content }) {
         <div className={styles.content}>
             {content.map(line => {
                 if(line.type == 'paragraph'){
-                    return <p>{line.content}</p>;
+                    return <p key={line.content}>{line.content}</p>;
                 } else if (line.type == 'link') {
-                    return <p><a href='#'>{line.content}</a></p>;
+                    return <p key={line.content}><a href='#'>{line.content}</a></p>;
                 }
             })}
+            {/* o key irá ser colocado no primeiro elemento que aparecer no retorno de um map */}
         </div>
 
         <form onSubmit={handleCreateNewComment} className={styles.commentForm}>
@@ -76,7 +77,7 @@ export default function Post({ author, publishedIn, content }) {
 
         <div className={styles.commentList}>
             {comments.map(comment => {
-                return <Comment content={comment} />
+                return <Comment key={comment} content={comment} />
             })}
         </div>
     </article>
